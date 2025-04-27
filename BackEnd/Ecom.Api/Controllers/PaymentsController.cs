843b5fa39d21960f83a1c35ea9869adfe3d32c2a
faris4271 <54688956+faris4271@users.noreply.github.com>
2025-04-28 00:11:38 +0300

    Add files via upload

---
+++ BackEnd/Ecom.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,63 @@
+﻿using Ecom.Core.Entities;
+using Ecom.Core.Entities.Order;
+using Ecom.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace Ecom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService paymentService;
+        public PaymentsController(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+        [Authorize]
+        [HttpPost("Create")]
+        public async Task<ActionResult<CustomerBasket>> create(string basketId, int? deliveryId)
+        {
+            return await paymentService.CreateOrUpdatePaymentAsync(basketId, deliveryId);
+        }
+        const string endpointSecret = "whsec_28cc3dec50be3eaba23c0d5217e31f075148d84948bb1e7aa84452952a3a9461";
+        [HttpPost("webhook")]
+        public async Task<IActionResult> UpdateStatusWithStripe()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                    Request.Headers["Stripe-Signature"], endpointSecret, throwOnApiVersionMismatch: false);
+                PaymentIntent intent;
+                Orders orders;
+                // Handle the event
+                if (stripeEvent.Type == Events.PaymentIntentPaymentFailed)
+                {
+                    intent = stripeEvent.Data.Object as PaymentIntent;
+                    orders = await paymentService.UpdateOrderFaild(intent.Id);
+                }
+                else if (stripeEvent.Type == Events.PaymentIntentSucceeded)
+                {
+                    intent = stripeEvent.Data.Object as PaymentIntent;
+                    orders = await paymentService.UpdateOrderSuccess(intent.Id);
+                }
+                // ... handle other event types
+                else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ BackEnd/Ecom.Api/Controllers/RatingsController.cs
@@ -0,0 +1,33 @@
+﻿using Ecom.Core.DTO;
+using Ecom.Core.interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Ecom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerBase
+    {
+        private readonly IRating rating;
+
+        public RatingsController(IRating rating)
+        {
+            this.rating = rating;
+        }
+        [HttpGet("get-rating/{productId}")]
+        public async Task<IActionResult> get(int productId)
+        {
+            var result = await rating.GetAllRatingForProductAsync(productId);
+            return Ok(result);
+        }
+        [HttpPost("add-rating")]
+        public async Task<IActionResult> add(RatingDTO ratings)
+        {
+            var email = User.FindFirst(ClaimTypes.Email)?.Value;
+            var result = await rating.AddRatingAsync(ratings, email);
+            return result ? Ok() : BadRequest();
+        }
+    }
+}
